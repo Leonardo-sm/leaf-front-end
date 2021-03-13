a97fd25808b6d5131569f271093e1a5c0812a023
Leonardo-sm <leonardo.smichelutti@gmail.com>
2021-03-13 15:32:38 -0300

    🔧 Fontawesome setup

---
+++ src/setup/fontawesome.ts
@@ -0,0 +1,5 @@
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(far, fas);
