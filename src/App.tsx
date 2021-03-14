9ffee918faca872259b353ccbf99a32144ae3cda
Leonardo-sm <leonardo.smichelutti@gmail.com>
2021-03-14 12:41:07 -0300

    💄 Added context theme  in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './setup/fontawesome';
 
 import GlobalStyled from './styles/GlobalStyles';
 import Routes from './routes';
+import { ThemeProvider } from 'styled-components';
+import light from './styles/themes/light';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={light}>
       <GlobalStyled />
       <Routes />
-    </>
+    </ThemeProvider>
   );
 }
 
