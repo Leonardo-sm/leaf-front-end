997e696d7d2ff9b226bfbbd3d03182faa24d8a8e
Renan Nunes <57843031+Renan-NO@users.noreply.github.com>
2021-03-14 17:05:23 -0300

    ✨ Added home route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import Demo from './pages/Demo';
+import { Home } from './pages/Home';
 import { Success } from './pages/Success';
 
 function Routes() {
@@ -10,6 +11,7 @@ function Routes() {
         <Route path="/" exact render={() => <Redirect to="/login" />} />
         <Route path="/login" exact component={Demo} />
         <Route path="/success" component={Success} />
+        <Route path="/home" component={Home} />
         {/* <Route path="/chat" exact component={} /> */}
       </Switch>
     </HashRouter>
