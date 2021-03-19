ae5bcfb22d59cb5ad7a244d483792d67a5e2360e
Renan Nunes <57843031+Renan-NO@users.noreply.github.com>
2021-03-18 21:17:51 -0300

    🐛 Fixing sidebar bug

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,13 +4,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export function Sidebar() {
   return (
     <SidebarContainer>
-      <button>
-        <FontAwesomeIcon icon={['far', 'comment-alt']} size="2x" />
-      </button>
+      <div>
+        <button>
+          <FontAwesomeIcon icon={['far', 'comment-alt']} size="2x" />
+        </button>
 
-      <button>
-        <FontAwesomeIcon icon={['fas', 'chart-bar']} size="2x" />
-      </button>
+        <button>
+          <FontAwesomeIcon icon={['fas', 'chart-bar']} size="2x" />
+        </button>
+      </div>
 
       <button>
         <FontAwesomeIcon icon={['fas', 'sign-out-alt']} size="2x" />
