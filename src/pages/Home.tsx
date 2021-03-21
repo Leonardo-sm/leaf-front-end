ca76130382a8a35156ce6fc144d77be966102116
Renan Nunes <57843031+Renan-NO@users.noreply.github.com>
2021-03-21 00:17:15 -0300

    ✨ Adding component menu in the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Sidebar } from '../components/Sidebar';
+import { Menu } from '../components/Menu';
 import { HomeContainer, HomeImages } from '../styles/pages/home';
 
 export function Home() {
   return (
     <HomeContainer>
       <Sidebar />
+      <Menu />
       <HomeImages>
         <img src="images/LeafHome.svg" alt="Leaf home logo" />
 
