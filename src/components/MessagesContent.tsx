a57190447939789218ccd20c95b7ae872686c523
Renan Nunes <57843031+Renan-NO@users.noreply.github.com>
2021-03-23 18:39:58 -0300

    💄 Added styling content

--- src/components/MessagesContent.tsx
+++ src/components/MessagesContent.tsx
@@ -1,27 +1,28 @@
+import {
+  ContentContainer,
+  MessageContainer,
+} from '../styles/components/content';
+
 export function MessagesContent() {
   return (
-    <div>
-      <div>
+    <ContentContainer>
+      <MessageContainer>
         <h2>Renan Nunes</h2>
 
         <div>
           <p>Está tudo certo por...</p>
           <p>13/03/21</p>
         </div>
-      </div>
+      </MessageContainer>
 
-      <div>
+      <MessageContainer>
         <h2>Leonardo Michelluti</h2>
 
         <div>
           <p>Eu vi algo estranh...</p>
           <p>02/03/21</p>
         </div>
-      </div>
-
-      <div>
-        <h2>Nikita Klementiev</h2>
-      </div>
-    </div>
+      </MessageContainer>
+    </ContentContainer>
   );
 }
