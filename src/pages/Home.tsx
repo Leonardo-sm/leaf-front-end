4f29d9368a146f9fd0b68434ce47d1c82c3fd12e
Renan Nunes <57843031+Renan-NO@users.noreply.github.com>
2021-03-30 23:01:54 -0300

    🚧 Adding state variable to chat component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
+import { Chat } from '../components/Chat';
 import { Sidebar } from '../components/Sidebar';
 import { HomeContainer, HomeImages } from '../styles/pages/home';
 
 export function Home() {
+  const [chatIsActive, setChatIsActive] = useState(true);
   return (
     <HomeContainer>
       <Sidebar />
-      <HomeImages>
-        <img src="images/LeafHome.svg" alt="Leaf home logo" />
 
-        <img src="images/Leaf.svg" alt="Leaf" />
-      </HomeImages>
+      {chatIsActive ? (
+        <Chat />
+      ) : (
+        <HomeImages>
+          <img src="images/LeafHome.svg" alt="Leaf home logo" />
+
+          <img src="images/Leaf.svg" alt="Leaf" />
+        </HomeImages>
+      )}
     </HomeContainer>
   );
 }
