50082555314fd451a3f829ea64a8ad1cacb3da39
Renan Nunes <57843031+Renan-NO@users.noreply.github.com>
2021-05-11 22:28:31 -0300

    ✨ Creating the method to list the users

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { RootState } from '../stores/store';
 import { asyncSignOut } from '../stores/sessionSlice';
+import { setConnectedUsers } from '../stores/chatSlice';
 import { Chat } from '../components/Chat';
 import socket from '../services/socket';
 
-type UserProps = {
+export type UserProps = {
   self: boolean;
-  id: string;
   userID: string;
   name: string;
   connected: boolean;
@@ -19,8 +19,8 @@ type UserProps = {
 
 export function Home() {
   const [chatIsActive, setChatIsActive] = useState(true);
-  const [connectedUsers, setConnectedUsers] = useState<UserProps[]>([]);
   const session = useSelector((state: RootState) => state.session);
+  const chat = useSelector((state: RootState) => state.chat);
   const history = useHistory();
   const dispatch = useDispatch();
 
@@ -31,34 +31,41 @@ export function Home() {
     }
   }, [session.isLogged, dispatch, history]);
 
+  useEffect(() => {
+    (() => {
+      socket.auth = { name: session.name };
+      socket.connect();
+    })();
+  }, [session.name]);
+
   useValidateLogin();
+
+  function setInitialProps(user: UserProps) {
+    user.connected = true;
+  }
 
   socket.on('users', (users: any) => {
     users.forEach((user: any) => {
       user.self = user.userID === socket.id;
       setInitialProps(user);
-
-      users.sort((a: UserProps, b: UserProps) => {
-        if (a.self) return -1;
-        if (b.self) return 1;
-        if (a.name < b.name) return -1;
-        return a.name > b.name ? 1 : 0;
-      });
-
-      setConnectedUsers(users);
     });
+
+    users.sort((a: UserProps, b: UserProps) => {
+      if (a.self) return -1;
+      if (b.self) return 1;
+      if (a.name < b.name) return -1;
+      return a.name > b.name ? 1 : 0;
+    });
+
+    dispatch(setConnectedUsers(chat.connectedUsers));
+    console.log(chat.connectedUsers);
   });
 
   socket.on('userConnected', (user: UserProps) => {
     setInitialProps(user);
-    setConnectedUsers([...connectedUsers, user]);
+    dispatch(setConnectedUsers([...chat.connectedUsers, user]));
+    console.log('passou por aqui');
   });
-
-  function setInitialProps(user: UserProps) {
-    user.id = user.userID;
-    user.name = user.name;
-    user.connected = true;
-  }
 
   return (
     <HomeContainer>
