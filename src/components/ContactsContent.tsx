65bc7cebbf162ee1aac98c75d052358901effc6f
Renan Nunes <57843031+Renan-NO@users.noreply.github.com>
2021-05-14 18:35:19 -0300

    ✨ Adding a function to select the user

--- src/components/ContactsContent.tsx
+++ src/components/ContactsContent.tsx
@@ -4,17 +4,30 @@ import {
 } from '../styles/components/content';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../stores/store';
 import { UserProps } from '../pages/Home';
+import { setIsChatActive, setSelectedUser } from '../stores/chatSlice';
 
 export function ContactsContent() {
   const chat = useSelector((state: RootState) => state.chat);
+  const dispatch = useDispatch();
+
+  function selectUser(user: UserProps) {
+    dispatch(setSelectedUser(user));
+    user.hasNewMessages = false;
+  }
 
   return (
     <ContentContainer>
       {chat.connectedUsers.map((user: UserProps) => (
-        <ContactContainer key={user.userID}>
+        <ContactContainer
+          onClick={() => {
+            dispatch(setIsChatActive(!chat.isChatActive));
+            selectUser(user);
+          }}
+          key={user.userID}
+        >
           <h2>{user.name}</h2>
 
           {user.connected ? (
