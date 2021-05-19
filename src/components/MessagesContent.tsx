037f8cbb2327facbaf140fc5bc71c3b9f4775a2e
Renan Nunes <57843031+Renan-NO@users.noreply.github.com>
2021-05-19 00:05:47 -0300

    ✨ Creating the function to list the users

--- src/components/MessagesContent.tsx
+++ src/components/MessagesContent.tsx
@@ -1,28 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { UserProps } from '../pages/Home';
+import { setIsChatActive, setSelectedUser } from '../stores/chatSlice';
+import { RootState } from '../stores/store';
 import {
   ContentContainer,
   MessageContainer,
 } from '../styles/components/content';
 
 export function MessagesContent() {
+  const chat = useSelector((state: RootState) => state.chat);
+  const dispatch = useDispatch();
+
+  function selectUser(user: UserProps) {
+    dispatch(setSelectedUser({ ...user, hasNewMessages: false }));
+  }
   return (
     <ContentContainer>
-      <MessageContainer>
-        <h2>Renan Nunes</h2>
+      {chat.connectedUsers.map((user: UserProps) => (
+        <MessageContainer
+          key={user.userID}
+          onClick={() => {
+            dispatch(setIsChatActive(!chat.isChatActive));
+            selectUser(user);
+          }}
+          self={user.self}
+        >
+          <h2>{user.name}</h2>
 
-        <div>
-          <p>Está tudo certo por...</p>
-          <p>13/03/21</p>
-        </div>
-      </MessageContainer>
-
-      <MessageContainer>
-        <h2>Leonardo Michelluti</h2>
-
-        <div>
-          <p>Eu vi algo estranh...</p>
-          <p>02/03/21</p>
-        </div>
-      </MessageContainer>
+          <div>
+            <p>eai?</p>
+            <p>18/05/2021</p>
+          </div>
+        </MessageContainer>
+      ))}
     </ContentContainer>
   );
 }
